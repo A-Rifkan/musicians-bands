fab7fd15e5863e149219b12f60d49e927d1a7649
A-Rifkan <a.rifkan2019@gmail.com>
2023-02-15 12:57:51 +0000

    Added tests and completed code for creating

--- Band.js
+++ Band.js
@@ -1,7 +1,11 @@
 const {Sequelize, sequelize} = require('./db');
 
 // TODO - define the Band model
-let Band;
+const Band = sequelize.define("Band", {
+    name: Sequelize.STRING,
+    genre: Sequelize.STRING
+})
+
 
 module.exports = {
     Band
