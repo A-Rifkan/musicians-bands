14992812b7762314a550fdfb4e1eeb6eac3e5136
A-Rifkan <a.rifkan2019@gmail.com>
2023-02-16 11:51:28 +0000

    DONE

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {Band} = require('./Band')
 const {Musician} = require('./Musician')
-
+Musician.belongsTo(Band);
+Band.hasMany(Musician);
 module.exports = {
     Band,
     Musician
--- index.test.js
+++ index.test.js
@@ -13,14 +13,73 @@ describe('Band and Musician Models', () => {
     })
 
     test('can create a Band', async () => {
-        // TODO - test creating a band
-        const testBand = await Band.create({ name: 'test1', genre: 'rock' });
-        expect(testBand.name).toBe("test1", "rock");
+        let createdBand = await Band.create({
+            name: "abc",
+            genre: "xyz"
+        })
+        expect(createdBand.genre).toBe("xyz");
+        expect(createdBand.name).toBe("abc");
     })
 
     test('can create a Musician', async () => {
-        // TODO - test creating a musician
-        const testMusician = await Musician.create({ name: 'test1', instrument: 'drums' });
-        expect(testMusician.name).toBe("test1", "drums");
+        let createdMusician = await Musician.create({
+            name: "abc",
+            instrument: "xyz"
+        })
+        expect(createdMusician.instrument).toBe("xyz");
+        expect(createdMusician.name).toBe("abc");
     })
+
+    test('can update band', async () => {
+        let createdBand= await Band.create({
+            name: "abc",
+            genre: "xyz"
+        })
+        let updatedBand = await createdBand.update({
+            name: "cba",
+            genre: "zyx"
+        })
+        expect(updatedBand.name).toBe("cba")
+        expect(updatedBand.genre).toBe("zyx")
+    })
+
+    describe('Musician and Band associations', () => {
+    test('Musician belongs to Band', async () => {
+    const band = await Band.create({ name: 'The Beatles' });
+    const musician = await Musician.create({
+      name: 'John Lennon',
+      instrument: 'Guitar',
+      BandId: band.id,
+    });
+
+    const fetchedMusician = await musician.reload();
+    expect(fetchedMusician.BandId).toBe(band.id);
+    const fetchedBand = await band.reload();
+    const musicians = await fetchedBand.getMusicians();
+    expect(musicians.length).toBe(1);
+    expect(musicians[0].name).toBe('John Lennon');
+    });
+
+    test('Band has many Musicians', async () => {
+    const band = await Band.create({ name: 'The Rolling Stones' });
+    const musician1 = await Musician.create({
+      name: 'Mick Jagger',
+      instrument: 'Vocals',
+      BandId: band.id,
+    });
+    
+    const musician2 = await Musician.create({
+      name: 'Keith Richards',
+      instrument: 'Guitar',
+      BandId: band.id,
+    });
+    
+    const fetchedBand = await band.reload();
+    const musicians = await fetchedBand.getMusicians();
+    expect(musicians.length).toBe(2);
+    expect(musicians.map((m) => m.name)).toContain('Mick Jagger');
+    expect(musicians.map((m) => m.name)).toContain('Keith Richards');
+  });
+});
+
 })
